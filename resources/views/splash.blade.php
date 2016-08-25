247655763c57682e3a581b62d258b421bbe5a495
Lawrie Cate <lawrie@catefamily.co.uk>
2016-08-24 21:51:25 -0500

    Additional colour options

--- resources/views/splash.blade.php
+++ resources/views/splash.blade.php
@@ -10,27 +10,27 @@
 		<div class="uk-grid" data-uk-grid-margin>
 		                <div class="uk-width-medium-1-1 uk-margin-large-top  " >
 		                	<h1 class="uk-hidden">Next Degree</h1>
-		                	<img class="" width="800" height="193" src="{{asset('assets/images/Logo_800x193.png')}}" alt="Next Degree Logo" />
+		                	<img class="" width="800" height="193" src="{{asset('assets/images/Logo_800x193_Purple.png')}}" alt="Next Degree Logo" />
 		                	<a class="uk-button uk-button-primary uk-float-right" href="{{url('home/')}}">Sign In <i class="uk-icon-arrow-right"></i></a>
 		                </div>
 		</div>
 	</div>
 </div>
-<div class="uk-width-1-1">
-	<div class="uk-container uk-container-center uk-margin-large-bottom">
+<div class="uk-width-1-1  nd-red-background">
+	<div class="uk-container uk-container-center ">
 		<div class="uk-grid " data-uk-grid-margin>
-		                <div class="uk-width-medium-1-1 uk-margin-top  ">
+		                <div class="uk-width-medium-1-1 uk-margin-top uk-margin-large-bottom uk-contrast ">
 		 
-		                	<h2 class="uk-heading-large">Jumpstart Your Career</h2>
-		                	<p class="uk-text-large uk-text-bold">Next Degree connects students and local businesses to help you build your career from the moment you start your degree through to graduation and beyond. </p><p>
-Through Next Degree you can use the skills you're developing at university in paid work experience or other work to help fund your degree. <br>From programming to tuition, Next Degree brings work to you.</p>
+		                	<h2 class="uk-heading-large ">Jumpstart Your Career</h2>
+		                	<p class="uk-text-large uk-text-bold">Next Degree connects students and local businesses to help develop student careers from freshers week to graduation and beyond. </p><p class="uk-text-large">
+You can use the skills you're developing at University to gain paid work experience, boosting your income and your CV. <br>From programming to tuition, Next Degree brings work to you.</p>
 		                	<h2>Sign up now with your university email</h2>
-		                	<p>Currently open to students of the University of Nottingham or Nottingham Trent University</p>
+		                	<p>Open to students of the University of Nottingham or Nottingham Trent University</p>
 		                	@if(Auth::guest())
 		                	<form method="post" action="{{action('QuickSignupController@makeUser')}}" id="signUpForm" class="uk-form">
 		                	{{ csrf_field() }}
 		                		<input id="signUpInput" type="email" class="uk-form-width-large uk-form-large" name="email">
-		                		<button  class="uk-button uk-button-large" type="submit">Sign Up <i class="uk-icon-arrow-right"></i></button>
+		                		<button  class="uk-button uk-button-large uk-button-primary" type="submit">Sign Up <i class="uk-icon-arrow-right"></i></button>
 		                	</form>
 		                	@if (count($errors) > 0)
 							    <div class="uk-alert uk-alert-danger">
@@ -74,8 +74,67 @@ Through Next Degree you can use the skills you're developing at university in pa
 		<div class="uk-grid" data-uk-grid-margin>
 		                <div class="uk-width-medium-1-1 uk-margin-top " >
 		                	<h1>Why waste your time?</h1>
-		                	<p>There are hundreds of businesses in Nottingham that need the skills you have, which could provide you with opportunities that boost your CV and income.</p>
-		                	<p>Next Degree will 
+		                	<p class="uk-text-large">There are hundreds of businesses in Nottingham that need the skills you have, which could provide you with lucrative opportunities.</p>
+		                	<p>Read the latest articles posted on Next Degree:</p>
+		                	{{--<ul>
+		                		<li><a href="#">Writing your CV 101</a></li>
+		                		<li><a href="#"></a></li>
+		                		<li><a href="#">August job ops in Nottingham</a></li>
+								<li><a href="#"></a></li>
+								<li><a href="#">Do I need a LinkedIn profile?</a></li>
+		                	</ul>--}}
+
+		                	<div class="uk-grid uk-grid-match" data-uk-grid-match="{target:'.uk-panel'}">
+		                	
+		                	
+		                	 <div class="uk-width-medium-1-4" >
+						        <div class="uk-panel-box" >
+							        <a href="#">
+							        <div class="uk-panel-teaser">
+							        	<img src="{{asset('assets/images/article.gif')}}"/>
+							        </div> 
+							        
+Exclusive Interview: Why I stayed in Nottingham after graduating as a law student</p>
+						        	</a>
+						        </div>
+						     </div>
+						         <div class="uk-width-medium-1-4" >
+						        <div class="uk-panel-box" >
+							        <a href="#">
+							        <div class="uk-panel-teaser">
+							        	<img src="{{asset('assets/images/article.gif')}}"/>
+							        </div> 
+							        
+						        	<p>Writing your CV 101</p>
+						        	</a>
+						        </div>
+</div>
+						         <div class="uk-width-medium-1-4" >
+						        <div class="uk-panel-box" >
+							        <a href="#">
+							        <div class="uk-panel-teaser">
+							        	<img src="{{asset('assets/images/article.gif')}}"/>
+							        </div> 
+							        
+						        	<p>Making the right impression: how to nail an interview</p>
+						        	</a>
+						        </div>
+						        </div>
+						         <div class="uk-width-medium-1-4" >
+						        <div class="uk-panel-box" >
+							        <a href="#">
+							        <div class="uk-panel-teaser">
+							        	<img src="{{asset('assets/images/article.gif')}}"/>
+							        </div> 
+							        
+						        	<p>Jobs Opportunities starting in Autumn 2016</p>
+						        	</a>
+						        </div>
+						        
+						    </div>
+		            
+						   </div>
+						    
 		                </div>
 		</div>
 	</div>
@@ -90,8 +149,8 @@ Through Next Degree you can use the skills you're developing at university in pa
 		<div class="uk-grid" data-uk-grid-margin>
 		                <div class="uk-width-medium-1-1 uk-margin-top ">
 		                	<h1>Sunny Days</h1>
-		                	<p>Working in a small business while studying, during your summer break or after graduating will let you develop a wider range of your skills in a friendly atmosphere.</p>
-		                	
+		                	<p class="uk-text-large">Working in a small business while studying, during your summer break or after graduating will let you develop a wider range of your skills in a friendly atmosphere.</p>
+		                	<blockquote class="uk-text-large">"Quote about working in a small business goes here."</blockquote> - Some Person
 		                </div>
 		</div>
 	</div>
