49202689b45f19231a58999bdce56ad2c80e5422
Lawrie Cate <lawrie@catefamily.co.uk>
2017-03-13 12:48:28 -0600

    added name to email field

--- resources/views/ndauth/signup_fromfb.blade.php
+++ resources/views/ndauth/signup_fromfb.blade.php
@@ -13,12 +13,9 @@
 </div>
 <form method="post" action="{{action('QuickSignupController@facebookEmailPromptSave')}}" class="uk-panel uk-panel-box uk-form">
 {{ csrf_field() }}
-	{{--<input id="signUpInput" type="email" class="uk-hidden" name="email" value="">
-	<input type="password" value="" class="uk-hidden">--}}
-
-
+	
              		<div class="uk-form-row">
-                        <input class="uk-width-1-1 uk-form-large" type="text" placeholder="Email">
+                        <input class="uk-width-1-1 uk-form-large" type="text" placeholder="Email" name="email">
                     </div>
                     
                     <div class="uk-form-row">
@@ -26,11 +23,8 @@
                         <button type="submit" class="uk-width-1-1 uk-button uk-button-primary uk-button-large" href="#" type="submit">Signup with Facebook <i class="uk-icon-facebook-square"></i></button>
                        
                     </div>
-                    {{--<div class="uk-form-row uk-text-small">
-                        <label class="uk-float-left"><input type="checkbox"> Remember Me</label>
-                        <a class="uk-float-right uk-link uk-link-muted" href="#">Forgot Password?</a>
-                    </div>--}}
+                   
 
 </form>
-<p><a href="{{action('QuickSignupController@namePrompt')}}" class="uk-button uk-button-small">Cancel</a></p>
+
 @endsection
